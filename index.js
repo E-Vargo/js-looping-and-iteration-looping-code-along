d9054b62029e8ac62c2a9079f0c8f0e9fb5ddf30
E-Vargo <vargoelliott@gmail.com>
2022-03-23 16:25:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, holiday){
+    const array1 = [];
+    for (let i = 0; i < array.length; i++) {
+        array1.push(`Thank you, ${array[i]}, for the wonderful ${holiday} gift!`);
+    }
+    return array1;
+}
+
+function countDown(num){
+    while (num > -1) {
+        console.log(num--)
+    }
+}
